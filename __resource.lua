99f9303a840c6cd8535f2beff0baf8b2abbd3d7c
ApexRP <43564381+ApexRP@users.noreply.github.com>
2021-02-18 15:37:49 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'peddamage.xml'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'bloodfx.dat'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'decals.dat'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'entityfx.dat'
+
+files {
+  'peddamage.xml',
+  'bloodfx.dat',
+  'decals.dat',
+  'entityfx.dat'
+}
